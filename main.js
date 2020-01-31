2fdd942ff599db413d9557ebd0d25eb0cb0ce238
Adam <Adamsmith6300@gmail.com>
2020-01-30 18:45:42 -0800

    small changes, comments and styling

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//toggle showing add artist form
 document.querySelector("#artistSearch .btn").addEventListener("click", function(){
     let form = document.querySelector(".addArtistForm");
     if (form.style.display == "none" || form.style.display == "") {
@@ -7,19 +9,24 @@ document.querySelector("#artistSearch .btn").addEventListener("click", function(
       }
 });
 
+
+//adding new artist
 document.querySelector(".addArtistForm .btn").addEventListener("click", function(){
     let form = document.querySelector(".addArtistForm");
     let formData = new FormData(form);
     var object = {};
     formData.forEach((value, key) => {object[key] = value});
     
+    //main div for artist item
     var artistDiv = document.createElement("div");
     artistDiv.className = "artist";
     
+    //image of artist from form url
     var imgNode = document.createElement("img");
     imgNode.className = "artistImage";
     imgNode.src = object["url"];
 
+    //artist info section
     var artistInfoDiv = document.createElement("div");
     artistInfoDiv.className = "artistInfo";
     
@@ -28,6 +35,7 @@ document.querySelector(".addArtistForm .btn").addEventListener("click", function
     var artistDesc = document.createElement("p");
     artistDesc.className = "artistDesc";
     
+    //delete artist button
     var delArtistBtn = document.createElement("button");
     delArtistBtn.addEventListener("click", function(e){
         var artist = e.target.parentNode
@@ -35,25 +43,27 @@ document.querySelector(".addArtistForm .btn").addEventListener("click", function
     });
     delArtistBtn.className = "deleteBtn";
 
+    //setting text of each tag
     var nameTextNode = document.createTextNode(object["name"]);
     var descTextNode = document.createTextNode(object["desc"]);
     var delBtnTextNode = document.createTextNode("Delete");
     
+    //appending all elements to dom in right order
     artistDiv.appendChild(imgNode);
     artistDiv.appendChild(artistInfoDiv);
     artistDiv.appendChild(delArtistBtn);
-
     artistInfoDiv.appendChild(artistName);
     artistInfoDiv.appendChild(artistDesc);
-    
     artistName.appendChild(nameTextNode);
     artistDesc.appendChild(descTextNode);
     delArtistBtn.appendChild(delBtnTextNode);
 
+    //adding new artist to artist list, reseting and closing form
     var element = document.querySelector("#artistList");
     element.appendChild(artistDiv);
     form.reset();
     form.style.display = "none";
+
 });
 
 document.querySelector(".deleteBtn").addEventListener("click", function(e){
